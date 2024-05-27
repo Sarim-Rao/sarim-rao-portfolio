c1388a851f6693458ca29ca57a3d1aff2ff36de3
sarim <sarimrana819@gmail.com>
2024-05-27 12:10:46 +0500

    - first commit

---
+++ src/data/user.js
@@ -0,0 +1,85 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Rao Sarim Ali",
+		name: "Rao Sarim Ali",
+		email: "sarimrana819@gmail.com",
+		logo: "../images/selfImage.jpg",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/Sarim-Rao",
+		linkedin: "https://www.linkedin.com/in/rao-sarim-ali-122982286/",
+		instagram: "https://www.instagram.com/raosarim7?igsh=Y3R3aXQ3MWlkcTQz",
+		stackoverflow: "https://stackoverflow.com/users/22685270/sarim-rao",
+		facebook:
+			"https://www.facebook.com/profile.php?id=100093400815661&mibextid=ZbWKwL",
+	},
+
+	homepage: {
+		title: "Software Engineer || Frontend Web Developer",
+		description:
+			"I am a frontend developer with expertise in React.js. I have experience in building responsive, user-friendly, and visually appealing web applications using various frameworks and technologies. My skills include HTML, CSS, SCSS, Tailwind, ShadCN, JavaScript, and Bootstrap. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Rao Sarim Ali. I live in Pakistan, where I design the future.",
+		description:
+			"I've recently started working on various projects, and I'm excited about the progress I'm making. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I'm working on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a fantastic way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Sarim-Rao",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/Sarim-Rao",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/Sarim-Rao",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Sarim-Rao",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Sarim-Rao",
+		},
+	],
+};
+
+export default INFO;
